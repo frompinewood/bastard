0124a5692c3938f22a49899f05af48bdbe71ed2f
Micaiah Parker <frompinewood@icloud.com>
2024-08-05 21:21:38 -0400

    added basic read/spawn

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,28 @@
 const std = @import("std");
+const util = @import("util.zig");
 const VM = @import("vm.zig").VM;
+const allocator = std.heap.page_allocator;
+
+const MAXSIZE = 0x10000;
 
 pub fn main() !void {
-    var vm = VM(u16).init(std.heap.page_allocator);
-    defer vm.deinit();
+    var args = std.process.args();
+    const bin_name = args.next(); // ignore the name of the program
+    const maybe_filename = args.next();
+    if (maybe_filename) |filename| {
+        const bytes = std.fs.cwd().readFileAlloc(allocator, filename, MAXSIZE) catch |err| {
+            switch (err) {
+                error.FileNotFound => std.debug.print("File not found: {s}\n", .{filename}),
+                else => std.debug.print("Something bad.\n", .{}),
+            }
+            std.process.exit(1);
+        };
+        var vm = VM(u16).init(allocator);
+        defer vm.deinit();
+        const new_bytes = try util.repack(allocator, u16, u8, bytes);
+        defer allocator.free(new_bytes);
+        try vm.spawn(new_bytes);
+    } else {
+        std.debug.print("usage: {s} <filename>\n", .{bin_name.?});
+    }
 }
